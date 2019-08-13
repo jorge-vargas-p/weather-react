5c44f6e4a56443952c54bfb7020505e46f67ea8e
Jorge-Vargas <jorjato18@gmail.com>
2019-08-13 16:11:23 +1200

    create header

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from './components/Header'
 
 function App() {
   return (
-    <h1>Anything</h1>
+    <div className="App">
+      <Header
+      title='Weather React App'
+      />
+
+    </div>
   );
 }
 
