dab718278848a4f81d14a4f72838ac2d4471a45a
Jorge-Vargas <jorjato18@gmail.com>
2019-08-15 15:40:01 +1200

    showing the validation error

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,24 +1,38 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
+import Error from "./components/Error";
 
 function App() {
   //main state
   // city = state, setCity = this.setState()
   const [city, setCity] = useState("");
   const [country, setCountry] = useState("");
+  const [error, setError] = useState(false);
 
   const dataCheck = data => {
     //validate both field exist
     if (data.city === "" || data.country === "") {
-      // a error
+      setError(true);
       return;
     }
 
     //city and country exist, add to the state
     setCity(data.city);
     setCountry(data.country);
+    setError(false);
   };
+
+  // loading a conditional component
+
+  let component;
+  if (error) {
+    // exist a error, show
+    component = <Error message="Both field are required" />;
+  } else {
+    //show error
+    component = null;
+  }
 
   return (
     <div className="App">
@@ -30,6 +44,7 @@ function App() {
             <div className="col s12 m6">
               <Form dataCheck={dataCheck} />
             </div>
+            <div className="col s12 m6">{component}</div>
           </div>
         </div>
       </div>
