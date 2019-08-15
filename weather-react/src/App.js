f47ca320e87095e405d0e3ef80ae0697c38eb18d
Jorge-Vargas <jorjato18@gmail.com>
2019-08-15 15:06:18 +1200

    validating form

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import Header from './components/Header'
-import Form from './components/Form'
-
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
+  //main state
+  // city = state, setCity = this.setState()
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+
+  const dataCheck = data => {
+    //validate both field exist
+    if (data.city === "" || data.country === "") {
+      // a error
+      return;
+    }
+
+    //city and country exist, add to the state
+    setCity(data.city);
+    setCountry(data.country);
+  };
+
   return (
     <div className="App">
-      <Header
-      title='Weather React App'
-      />
+      <Header title="Weather React App" />
 
       <div className="contenedor-form">
         <div className="container">
           <div className="row">
             <div className="col s12 m6">
-              <Form />
+              <Form dataCheck={dataCheck} />
             </div>
           </div>
         </div>
       </div>
-
-
     </div>
   );
 }
--- weather-react/src/components/Form.jsx
+++ weather-react/src/components/Form.jsx
@@ -1,42 +1,58 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function Form(){
+function Form({ dataCheck }) {
+  // component state
+  const [serch, saveSerch] = useState({
+    city: "",
+    country: ""
+  });
 
-    const handleChange = e => {
-        //change state
-    }
+  const handleChange = e => {
+    //change state
+    saveSerch({
+      ...serch,
+      [e.target.name]: e.target.value
+    });
+    console.log(serch);
+  };
 
-    return(
-        <form>
-            <div className="input-field col s12">
-                <input
-                type="text"
-                name="city"
-                id="city"
-                onChange={handleChange}
-                />
-                <label htmlFor="city">City: </label>
-            </div>
+  const checkWeather = e => {
+    e.preventDefault();
 
-            <div className="input-field col s12">
-                <select onChange={handleChange} name="country">
-                    <option value="">Select a country</option>
-                    <option value="NZ">New Zealand</option>
-                    <option value="CL">Chile</option>
-                    <option value="US">United States of America</option>
-                    <option value="AU">Australia</option>
-                    <option value="BR">Brazil</option>
-                    <option value="AQ">Antarctica</option>
-                    <option value="ES">Spain</option>
-                </select>
-            </div>
+    // Giving to the main component the user serch
+    dataCheck(serch);
+  };
 
-            <div className="input-field col s12">
-                <input type="submit" className="waves-effect waves-light btn-large
-                btn-block yellow accent-4" value="Serch Weather"/>
-            </div>
-        </form>
-    )
+  return (
+    <form onSubmit={checkWeather}>
+      <div className="input-field col s12">
+        <input type="text" name="city" id="city" onChange={handleChange} />
+        <label htmlFor="city">City: </label>
+      </div>
+
+      <div className="input-field col s12">
+        <select onChange={handleChange} name="country">
+          <option value="">Select a country</option>
+          <option value="NZ">New Zealand</option>
+          <option value="CL">Chile</option>
+          <option value="US">United States of America</option>
+          <option value="AU">Australia</option>
+          <option value="BR">Brazil</option>
+          <option value="AQ">Antarctica</option>
+          <option value="ES">Spain</option>
+        </select>
+      </div>
+
+      <div className="input-field col s12">
+        <input
+          type="submit"
+          className="waves-effect waves-light btn-large
+                btn-block yellow accent-4"
+          value="Serch Weather"
+        />
+      </div>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
